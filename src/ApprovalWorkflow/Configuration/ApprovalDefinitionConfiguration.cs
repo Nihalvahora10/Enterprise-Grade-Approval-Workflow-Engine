de7eae2942621223fef8171eed42f71907606b57
Nihalvahora10 <nihalvahora1001@gmail.com>
2025-10-30 15:36:16 -0400

    Update ApprovalDefinitionConfiguration

--- src/ApprovalWorkflow/Configuration/ApprovalDefinitionConfiguration.cs
+++ src/ApprovalWorkflow/Configuration/ApprovalDefinitionConfiguration.cs
@@ -1,19 +1,1 @@
-﻿using ApprovalWorkflow.Entities;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace ApprovalWorkflow.Configuration
-{
-    public class ApprovalDefinitionConfiguration
-        : IEntityTypeConfiguration<ApprovalDefinition>
-    {
-        public void Configure(EntityTypeBuilder<ApprovalDefinition> builder)
-        {
-            builder.HasKey(property => property.Id);
-            builder.Property(property => property.Name)
-                .IsRequired()
-                .HasMaxLength(150);
-            builder.HasMany(property => property.ApprovalSteps);
-        }
-    }
-}
