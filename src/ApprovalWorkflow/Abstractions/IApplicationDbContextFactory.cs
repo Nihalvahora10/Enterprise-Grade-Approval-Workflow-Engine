2968280dd0aa7a7d10ed37ce9e2a7e2d8df70496
Nihalvahora10 <nihalvahora1001@gmail.com>
2025-11-12 11:20:47 -0500

    Update IApplicationDbContextFactory

--- src/ApprovalWorkflow/Abstractions/IApplicationDbContextFactory.cs
+++ src/ApprovalWorkflow/Abstractions/IApplicationDbContextFactory.cs
@@ -1,1 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApprovalWorkflow.Abstractions
+{
+    public interface IApplicationDbContextFactory
+    {
+        ApplicationDbContext GetDbContext();
+    }
+}
+
+
 
