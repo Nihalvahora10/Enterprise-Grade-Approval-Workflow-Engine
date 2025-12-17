d27a9bf8e50ed18277e260dbe052600210f5bd29
Nihalvahora10 <nihalvahora1001@gmail.com>
2025-12-17 17:08:56 -0500

    Update IApprovalDefinitionService

--- src/ApprovalWorkflow/Abstractions/IApprovalDefinitionService.cs
+++ src/ApprovalWorkflow/Abstractions/IApprovalDefinitionService.cs
@@ -1,1 +1,17 @@
+﻿using ApprovalWorkflow.Entities;
+
+namespace ApprovalWorkflow.Abstractions
+{
+    public interface IApprovalDefinitionService
+    {
+        Task<ApprovalDefinition> SaveDefinitionAsync(
+            ApprovalDefinition approvalDefinition,
+            CancellationToken cancellationToken = default);
+
+        Task<ApprovalDefinition> DeleteDefinitionAsync(
+            Guid id,
+            CancellationToken cancellationToken = default);
+    }
+}
+
 
